c6ef6d27d589f7ef0f6b1822995e3c6b94392022
Serhat Durak <serhatdurak@Serhats-MacBook-Air.local>
2023-10-10 13:35:53 +0300

    final

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,159 +1,211 @@
-import React, { useState, useEffect, useReducer, useRef } from "react";
-import { View, FlatList, ScrollView, StyleSheet, } from "react-native";
-import { BleManager } from "react-native-ble-plx";
-import {
-  Layout,
-  Text,
-  Section,
-  SectionContent,
-  Button
-} from "react-native-rapi-ui";
+import React, { Fragment, useCallback, useEffect, useState } from 'react';
+import { View, PermissionsAndroid, ScrollView, RefreshControl } from 'react-native';
+import { Layout, RadioButton, Text, Button, TopNav, SectionContent, Section, SectionImage } from 'react-native-rapi-ui';
+import RNBluetoothClassic, { BTEvents, BTCharsets, BluetoothEventType, BluetoothDevice } from 'react-native-bluetooth-classic';
+import { Ionicons } from '@expo/vector-icons';
 
-let initialState = {
-  isScanning: false,
-  scanDone: false,
-  bleReady: false,
-  items: [],
-};
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "setBLEReady":
-      return { ...state, bleReady: true };
-
-      break;
-    case "setBLEScan":
-      return { ...state, isScanning: action.payload };
-    case "addItem":
-      let items = state.items;
-
-      if (items.findIndex((x) => x.id == action.payload.id) == -1) {
-        items.push(action.payload);
-      }
-
-      return { ...state, items: items };
-    case "stopScan":
-      return { ...state, isScanning: false, scanDone: true };
-    case "reset":
-      return {
-        ...state,
-        items: [],
-        bleReady: true,
-        isScanning: false,
-        scanDone: false,
-      };
-
-    default:
-      return state;
-  }
-};
-const manager = new BleManager();
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN)
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT)
 
 export default function Home() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  console.log(state)
-  const stopBLEScan = () => {
-    manager.stopDeviceScan();
-    dispatch({ type: "stopScan" });
-  };
+	const [connectedDevice, setConnectedDevice] = useState({})
+	const [deviceAddress, setDeviceAddress] = useState('');
+	const [bondedDevice, setBondedDevice] = useState(false);
+	const [deviceName, setDeviceName] = useState('');
+	const [discovery, setDiscovery] = useState({});
+	const [isDeviceBluetoothSupport, setIsDeviceBluetoothSupport] = useState(true);
+	const [isBluetoothEnabled, setIsBluetoothEnabled] = useState(false);
+	const [refreshing, setRefreshing] = useState(false);
+	const [isLambOn, setIsLambOn] = useState(false);
+	const [isLambOff, setIsLambOff] = useState(false);
+	const [loading, setLoading] = useState(false);
 
-  const timerRef = useRef(null);
+	useEffect(() => {
+		currentConnectedDevices();
+		startDiscovery();
+		RNBluetoothClassic.isBluetoothAvailable().then((res) => {
+			setIsDeviceBluetoothSupport(res)
+		})
+		RNBluetoothClassic.isBluetoothEnabled().then((res) => {
+			setIsBluetoothEnabled(res)
+		});
+	}, [refreshing]);
 
-  const scanAndConnect = () => {
-    manager.startDeviceScan(null, null, (error, device) => {
-      dispatch({ type: "setBLEScan", payload: true });
-      if (error) {
-        // Handle error (scanning will be stopped automatically)
-        return;
-      }
-      console.log(device)
-      dispatch({ type: "addItem", payload: device });
-    });
-  };
+	const onRefresh = useCallback(() => {
+		setRefreshing(true);
+		setTimeout(() => {
+			setRefreshing(false);
+		}, 2000)
 
-  useEffect(() => {
-    if (state.bleReady && !state.isScanning && !state.scanDone) {
-      scanAndConnect();
-    }
+	}, [])
 
-    if (state.scanTimeout <= 0 && state.isScanning && !state.scanDone) {
-      dispatch({ type: "stopScan" });
-    }
-  }, [state]);
+	async function requestAccessFineLocationPermission() {
+		const granted = await PermissionsAndroid.request(
+			PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+			{
+				title: 'Access fine location required for discovery',
+				message:
+					'In order to perform discovery, you must enable/allow ' +
+					'fine location access.',
+				buttonNeutral: 'Ask Me Later"',
+				buttonNegative: 'Cancel',
+				buttonPositive: 'OK'
+			}
+		);
+		return granted === PermissionsAndroid.RESULTS.GRANTED;
+	}
 
-  useEffect(() => {
-    timerRef.current = setTimeout(() => {
-      stopBLEScan();
-    }, 1000 * 10);
+	const startDiscovery = async () => {
+		try {
+			const granted = await requestAccessFineLocationPermission();
 
-    return () => {
-      if (timerRef.current) clearTimeout(timerRef.current);
-    };
-  }, []);
+			if (!granted) {
+				throw new Error(`Access fine location was not granted`);
+			}
 
-  useEffect(() => {
-    const subscription = manager.onStateChange((state) => {
-      if (state === "PoweredOn") {
-        dispatch({ type: "setBLEReady" });
-        subscription.remove();
-      }
-    }, true);
-    return () => subscription.remove();
-  }, [manager]);
+			setDiscovery({ discovering: true });
 
-  const renderItem = ({ item }) => {
-    return (
-      <View style={tw`p-2 border rounded-md m-2`}>
-        <Text style={tw`text-[24px]`}>{item.name || "No name found"}</Text>
-        <Text style={tw`text-[12px]`}>{item.id}</Text>
-      </View>
-    );
-  };
+			try {
+				const unpaired = await RNBluetoothClassic.startDiscovery();
+				console.log("Found " + unpaired.length + " unpaired devices")
+			} finally {
+				setDiscovery({ devices, discovering: false });
+			}
+		} catch (err) {
+			console.log(err.message)
+		}
+	}
 
-  return (
-    <Layout>
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          marginHorizontal: 20,
-        }}
-      >
-        <Section>
-          <SectionContent>
-            <Text fontWeight="bold" style={{ textAlign: "center" }}>
-              Welcome to the Wakeup Sunshine
-            </Text>
-            {/* <Button
-              text="Go to second screen"
-              onPress={() => {
-                navigation.navigate("SecondScreen");
-              }}
-              style={{
-                marginTop: 10,
-              }}
-            /> */}
-              <View>
-            <Text >
-              Available BLE devices in your area
-            </Text>
-          </View>
-          <FlatList
-            refreshing={state.isScanning}
-            onRefresh={() => dispatch({ type: "reset" })}
-            renderItem={renderItem}
-            data={state.items}
-          />
-            <Button
-              text="Turn On"
-            onPress={scanAndConnect}
-              style={{
-                marginTop: 10,
-              }}
-            />
-          </SectionContent>
-        </Section>
-      </View>
-    </Layout>
-  );
+	async function currentConnectedDevices() {
+		try {
+			await RNBluetoothClassic.getBondedDevices().then((res) => {
+				res.forEach(s => setDeviceAddress(s.address))
+				res.forEach(s => setDeviceName(s.name))
+				res.forEach(s => setBondedDevice(s.bonded))
+				setConnectedDevice(res);
+			})
+		} catch (err) {
+			console.log("Bonded devices: " + err)
+		}
+	}
+
+	async function turnOnLamb() {
+		try {
+			(await RNBluetoothClassic.connectToDevice(deviceAddress)).write("CMD:1,1", "utf-8").then((res) => {
+				setLoading(true);
+				setIsLambOn(false);
+				setLoading(false);
+				setIsLambOff(true);
+			}).catch((err) => {
+				console.log(err)
+			})
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	async function turnOffLamb() {
+		try {
+			(await RNBluetoothClassic.connectToDevice(deviceAddress)).write("CMD:1,0", "utf-8").then((res) => {
+				setLoading(true);
+				setIsLambOff(res);
+				setLoading(false);
+				setIsLambOn(true)
+			}).catch((err) => {
+				console.log(err)
+			})
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	function enableBluetooth() {
+		try {
+			RNBluetoothClassic.openBluetoothSettings();
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	return (
+		<Fragment>
+			{isDeviceBluetoothSupport ? <Layout>
+				<ScrollView refreshControl={
+					<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+				}>
+					<TopNav middleContent="Home" rightContent={isLambOn ? "🔴" : "🟢"} rightTextStyle={{textAlign: "center"}} />
+					<View style={{
+						padding: 30
+					}}>
+						<Section>
+							<SectionContent>
+								<Text style={{ fontWeight: "700" }} size='h2'>Hello</Text>
+								<Text>Welcome to ~{"\n"}<Text style={{ fontWeight: "700", fontSize: 20 }}>Wakeup Sunshine</Text></Text>
+								<Text style={{ fontSize: 10, paddingTop: 10, fontStyle: "italic" }}>More information, you can go About section</Text>
+							</SectionContent>
+						</Section>
+
+					</View>
+					<View
+						style={{
+							alignItems: 'center',
+							justifyContent: 'center',
+						}}
+					>
+						{isBluetoothEnabled ? <Section>
+							<SectionContent>
+								{isLambOn ?
+									<Button
+										text={"Turn On"}
+										onPress={turnOnLamb}
+										style={{
+											marginTop: 10,
+											height: 290,
+											borderRadius: 1000,
+											minWidth: 300
+										}}
+										disabled={loading}
+										color='#c43042'
+										rightContent={
+											<Ionicons name="flash" size={20} color="white" />
+										}
+									/> : <Button
+										text={"Turn Off"}
+										onPress={turnOffLamb}
+										style={{
+											marginTop: 10,
+											height: 290,
+											borderRadius: 1000,
+											minWidth: 300
+										}}
+										disabled={loading}
+										color='#c43042'
+										rightContent={
+											<Ionicons name="flash-off" size={20} color="white" />
+										}
+									/>}
+
+
+							</SectionContent>
+						</Section> :
+							<Section>
+								<SectionContent>
+									<Button
+										text="Enable Bluetooth"
+										onPress={enableBluetooth}
+										style={{
+											marginTop: 10,
+										}}
+										color='#c43042'
+										rightContent={
+											<Ionicons name="power" size={20} color="white" />
+										} />
+								</SectionContent>
+							</Section>
+						}
+
+					</View>
+				</ScrollView>
+
+			</Layout> : <Text>Your device not support bluetooth</Text>}
+		</Fragment>
+
+	);
 }
